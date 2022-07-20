bdbad9ee927987e6fd209e065a09ebda47085272
Chris Becker <chrisb@velocitycubed.co.za>
2022-07-20 12:29:41 +0200

    Changing Pod file

--- push-notifications/rollup.config.js
+++ push-notifications/rollup.config.js
@@ -4,7 +4,7 @@ export default {
     {
       file: 'dist/plugin.js',
       format: 'iife',
-      name: 'capacitorPushNotifications',
+      name: 'velocitycubedPushNotifications',
       globals: {
         '@capacitor/core': 'capacitorExports',
       },
